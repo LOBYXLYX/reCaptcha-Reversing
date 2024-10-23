2fbfdeed78f9b412513071657d97d9efcb6aba7e
ppatito☭ <85974421+LOBYXLYX@users.noreply.github.com>
2024-10-23 00:08:53 -0500

    Add files via upload

---
+++ cb_param.js
@@ -0,0 +1,19 @@
+
+
+
+
+w = {
+    content: 'Presiona el centro de los objetos en la imagen según las instrucciones anteriores. Si el desafio no es claro o deseas obtener uno nuevo, vuelve a cargalo.<a href="https://support.google.com/recaptcha" target="_blank">Más information</a>.'
+};
+
+
+function r(I) {
+    return ((a = [7, 0, 55], (I + 1 & a[2]) >= I && (I - a[0] || 34) < I ) && (undefined = X | a[1], undefined = w | a[1]), (I | 40) == I && (c = w), I >> 1) & 9 || (c = Date.now()), c
+}
+
+
+function cb(I = 4) {
+    return I << (((C = [18, 46, null], I) & 123) == I && (c = undefined, c != C[2] && null), 1) & 7 || (V = Math.floor(Math.random() * 2147483648).toString(36) + Math.abs(Math.floor(Math.random() * 2147483648) ^ r(9)).toString(36)), V
+}
+
+console.log(cb())
